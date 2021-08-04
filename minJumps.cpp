b4802b0454f205313ad72e14b0f8fc5682323940
Uhini Mukherjee <59994163+Uhini0201@users.noreply.github.com>
2021-08-05 01:44:07 +0530

    Create minJumps.cpp

---
+++ minJumps.cpp
@@ -0,0 +1,69 @@
+'''
+Given an array of non-negative integers, A, of length N, you are initially positioned at the first index of the array.
+
+Each element in the array represents your maximum jump length at that position.
+
+Return the minimum number of jumps required to reach the last index.
+
+If it is not possible to reach the last index, return -1.
+
+Input Format:
+
+The first and the only argument contains an integer array, A.
+Output Format:
+
+Return an integer, representing the answer as described in the problem statement.
+Constraints:
+
+1 <= N <= 1e6
+0 <= A[i] <= 50000
+Examples:
+
+Input 1:
+    A = [2, 1, 1]
+
+Output 1:
+    1
+    
+Explanation 1:
+    The shortest way to reach index 2 is
+        Index 0 -> Index 2
+    that requires only 1 jump.
+
+Input 2:
+    A = [2,3,1,1,4]
+
+Output 2:
+    2
+
+Explanation 2:
+    The shortest way to reach index 4 is
+        Index 0 -> Index 1 -> Index 4
+    that requires 2 jumps.
+'''
+
+
+# Code
+
+int Solution::jump(vector<int> &A) {
+    int n=A.size();
+    if(n==1) return 0;
+    //THE CURRENT MAXIMUM REACH
+    //step is the each time the required step to minimum jumps
+    int max_reach=A[0],step=A[0],jump=1;
+    for(int i=1;i<n-1;i++){
+        //the maximum distance to minimise the jumps
+        max_reach=max(max_reach,i+A[i]);
+        step--;
+        //if step is 0 then return -1 as it wont go further
+        if(step<0) return -1;
+        //if step is 1 the jump one time and i steps are reduced from maximum reach
+        if(step==0){
+         jump++;
+         step=max_reach-i; 
+        }
+    }
+    //if reached the end
+    if(max_reach>=n-1) return jump;
+    return -1;
+}
