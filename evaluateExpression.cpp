af3d7316772f239e5b581afdc575fee73e4d144b
Uhini Mukherjee <59994163+Uhini0201@users.noreply.github.com>
2021-08-13 21:34:41 +0530

    Create evaluateExpression.cpp

---
+++ evaluateExpression.cpp
@@ -0,0 +1,76 @@
+'''
+/*
+
+Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+
+Valid operators are +, -, *, /. Each operand may be an integer or another expression.
+
+
+
+
+Input Format
+
+The only argument given is character array A.
+Output Format
+
+Return the value of arithmetic expression formed using reverse Polish Notation.
+For Example
+
+Input 1:
+    A =   ["2", "1", "+", "3", "*"]
+Output 1:
+    9
+Explaination 1:
+    starting from backside:
+    *: ( )*( )
+    3: ()*(3)
+    +: ( () + () )*(3)
+    1: ( () + (1) )*(3)
+    2: ( (2) + (1) )*(3)
+    ((2)+(1))*(3) = 9
+    
+Input 2:
+    A = ["4", "13", "5", "/", "+"]
+Output 2:
+    6
+Explaination 2:
+    +: ()+()
+    /: ()+(() / ())
+    5: ()+(() / (5))
+    1: ()+((13) / (5))
+    4: (4)+((13) / (5))
+    (4)+((13) / (5)) = 6
+
+*/
+'''
+
+CODE
+
+int Solution::evalRPN(vector<string> &A) {
+    stack<int> s;
+    for(auto i:A){
+        if(i=="+" || i=="-" || i=="*" || i=="/"){
+            int b=s.top();
+            s.pop();
+            int a=s.top();
+            s.pop();
+            if(i=="+"){
+                s.push(a+b);
+            }
+            if(i=="-"){
+                s.push(a-b);
+            }
+            if(i=="*"){
+                s.push(a*b);
+            }
+            if(i=="/"){
+                s.push(a/b);
+            }
+        }
+        else{
+            s.push(stoi(i));
+        }
+    }
+    return s.top();
+}
+
