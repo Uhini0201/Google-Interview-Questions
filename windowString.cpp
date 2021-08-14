b7aab1baf69ea73a62bc0b8280457417051834c9
Uhini Mukherjee <59994163+Uhini0201@users.noreply.github.com>
2021-08-14 12:28:09 +0530

    Update windowString.cpp

--- windowString.cpp
+++ windowString.cpp
@@ -19,71 +19,65 @@ If there are multiple such windows, return the first occurring minimum window ( 
 */
 '''
 
-CODE
+<h1>CODE</h1>
 
 string Solution::minWindow(string A, string B) {
 
-string ans;
+    string ans;
 
-if(B.length()>A.length())
-//return null string
-return ans;
+    if(B.length()>A.length())
+    //return null string
+        return ans;
 
-//keeping the count of the final string to be searched
-unordered_map<char,int> m;
-for(auto c: B)
-m[c]++;
+    //keeping the c of the final string to be searched
+    unordered_map<char,int> m;
+    for(auto c: B)
+        m[c]++;
 
-int n=B.length();
-int max=INT_MAX;
-int p1=0,p2=0,flag=0;
-unordered_map<char,int> freq;
-int start=0,end=0,count=0;
+    int n=B.length();
+    int maxi=INT_MAX;
+    int final_s=0,final_e=0,flag=0;
+    unordered_map<char,int> f;
+    int l=0,r=0,c=0;
 
-for(int i=0; i<A.length(); i++)
-{
-    if(m.find(A[i])!=m.end())
-    {
-      //updating the frequency when b is present at a
-        freq[A[i]]++;
-        //if freq of b is less than a , we ommit that b and search for a new one
-        if(freq[A[i]]<=m[A[i]])
-        {count++;}
-    }
-    //if we have got all the characters of b in a
-    if(count==n)
-    {
-        flag++;
-        //we iterate from the start of the current substring
-        while(start<end)
-        {
-            if(freq.find(A[start])!=freq.end())
-            {
-              //if found that he freq is more than the required, we reduce it 
-              //so as to include only those needed.
-                if(freq[A[start]]>m[A[start]])
-                freq[A[start]]--;
-                else
-                break;
+    for(int i=0; i<A.length(); i++){
+        if(m.find(A[i])!=m.end()){
+        //updating the fuency when b is present at a
+            f[A[i]]++;
+            //if f of b is less than a , we ommit that b and search for a new one
+            if(f[A[i]]<=m[A[i]]){
+                c++;
             }
-            //this will execute untl we get the freq same as that of required 
-            //or tleast the minumum greatest
-            start++;
         }
-        if(max>(end-start+1))
-        {
-          //we keep a note of the start and end of the substring
-            max=end-start+1;
-            p1=start;
-            p2=end;
+        //if we have got all the characters of b in a
+        if(c==n){
+            flag++;
+            //we iterate from the l of the current substring
+            while(l<r){
+                if(f.find(A[l])!=f.end()){
+                //if found that he f is more than the required, we reduce it 
+                //so as to include only those needed.
+                    if(f[A[l]]>m[A[l]])
+                        f[A[l]]--;
+                    else
+                        break;
+                }
+                //this will execute untl we get the f same as that of required 
+                //or tleast the minumum greatest
+                l++;
+            }
+            if(maxi>(r-l+1)){
+            //we keep a note of the l and r of the substring
+                maxi=r-l+1;
+                final_s=l;
+                final_e=r;
+            }
         }
+        r++;
     }
-    end++;
-}
-if(flag==0)
-return ans;
-for(int i=p1; i<=p2; i++)
-ans.push_back(A[i]);
-
-return ans;
+    if(flag==0)
+        return ans;
+    for(int i=final_s; i<=final_e; i++)
+        ans.push_back(A[i]);
+    return ans;
 }
